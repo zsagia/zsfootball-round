eda7e0afdab7d53759f1bc9cd04577d3843cbd4d
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-04 22:00:29 +0200

    Adds test file

---
+++ test/ZsFootballRound.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import ZsFootballRound from '../src/ZsFootballRound';
+
+describe('ZsFootballRound', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
