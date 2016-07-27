20cdcd8e2b4ba559ec1264a9d47bdd17cf0d9219
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-07-27 12:19:48 +0200

    Added and refactored demo data

---
+++ demos/hajdu-bihar-201516.js
@@ -0,0 +1,197 @@
+var hajdu_bihar_201516_01 = [
+	{
+		id: 3001,
+		homeClub: {title: 'Biharnagybajomi SE'},
+		awayClub: {title: 'Bakonszegi SE'},
+		goals: {
+			homeGoals: {
+				firstHalf: 0,
+				secondHalf: 4
+			},
+			awayGoals: {
+				firstHalf: 0,
+				secondHalf: 0
+			}
+		},
+		matchDate: 1408708800000,
+		round: 1,
+		location: {city: 'Biharnagybajom', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		id: 3002,
+		homeClub: {title: 'Körösszakál SE'},
+		awayClub: {title: 'Köröszsegapáti SE'},
+		goals: {
+			homeGoals: {
+				firstHalf: 3,
+				secondHalf: 1
+			},
+			awayGoals: {
+				firstHalf: 0,
+				secondHalf: 0
+			}
+		},
+		matchDate: 1408708800000,
+		round: 1,
+		location: {city: 'Körösszakál', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		id: 3003,
+		homeClub: {title: 'Zsáka SE'},
+		awayClub: {title: 'Csökmői SE'},
+		goals: {
+			homeGoals: {
+				firstHalf: 0,
+				secondHalf: 0
+			},
+			awayGoals: {
+				firstHalf: 0,
+				secondHalf: 3
+			}
+		},
+		matchDate: 1408708800000,
+		round: 1,
+		location: {city: 'Zsáka', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		id: 3004,
+		homeClub: {title: 'Blondi FC Esztár'},
+		awayClub: {title: 'Mikepércsi FK'},
+		goals: {
+			homeGoals: {
+				firstHalf: 0,
+				secondHalf: 0
+			},
+			awayGoals: {
+				firstHalf: 0,
+				secondHalf: 1
+			}
+		},
+		matchDate: 1408708800000,
+		round: 1,
+		location: {city: 'Esztár', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		id: 3005,
+		homeClub: {title: 'Újszentmargitai GYSE'},
+		awayClub: {title: 'Hortobágyi LSE'},
+		goals: {
+			homeGoals: {
+				firstHalf: 4,
+				secondHalf: 1
+			},
+			awayGoals: {
+				firstHalf: 1,
+				secondHalf: 2
+			}
+		},
+		matchDate: 1408795200000,
+		round: 1,
+		location: {city: 'Újszentmargita', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		homeClub: {title: 'DVKLSZ'},
+		awayClub: {title: 'Bihartorda KSE'},
+		goals: {
+			homeGoals: {
+				firstHalf: 3,
+				secondHalf: 1
+			},
+			awayGoals: {
+				firstHalf: 2,
+				secondHalf: 1
+			}
+		},
+		matchDate: 1408795200000,
+		round: 1,
+		location: {city: 'DVKLSZ', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		homeClub: {title: 'Mezősas SE'},
+		awayClub: {title: 'Hajdúbagosi SE'},
+		goals: {
+			homeGoals: {
+				firstHalf: 0,
+				secondHalf: 2
+			},
+			awayGoals: {
+				firstHalf: 1,
+				secondHalf: 0
+			}
+		},
+		matchDate: 1408795200000,
+		round: 1,
+		location: {city: 'Mezősas', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	}
+];
+
+var hajdu_bihar_201516_02 = [
+	{
+		id: 3011,
+		homeClub: {title: 'Újszentmargitai GYSE'},
+		awayClub: {title: 'Körösszakál SE'},
+		matchDate: 1408708800000,
+		round: 1,
+		location: {city: 'Újszentmargita', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		id: 3012,
+		homeClub: {title: 'Bakonszeg'},
+		awayClub: {title: 'Hortobágyi LSE'},
+		matchDate: 1408708800000,
+		round: 1,
+		location: {city: 'Bakonszeg', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		id: 3013,
+		homeClub: {title: 'Hajdúbagosi SE'},
+		awayClub: {title: 'Biharnagybajomi SE'},
+		matchDate: 1408708800000,
+		round: 1,
+		location: {city: 'Zsáka', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		id: 3014,
+		homeClub: {title: 'Bihartorda KSE'},
+		awayClub: {title: 'Mezősas SE'},
+		matchDate: 1408708800000,
+		round: 1,
+		location: {city: 'Bihartorda', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		id: 3015,
+		homeClub: {title: 'DVKLSZ'},
+		awayClub: {title: 'Mikepércsi FK'},
+		matchDate: 1408795200000,
+		round: 1,
+		location: {city: 'DVKLSZ', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		homeClub: {title: 'Csökmői SE'},
+		awayClub: {title: 'Blondy FC Esztár'},
+		matchDate: 1408795200000,
+		round: 1,
+		location: {city: 'Csökmő', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	},
+	{
+		homeClub: {title: 'Köröszsegapáti SE'},
+		awayClub: {title: 'Zsáka SE'},
+		matchDate: 1408795200000,
+		round: 1,
+		location: {city: 'Köröszsegapáti', name: 'Focipálya'},
+		type: {name: 'HajdúMegyei III. o.'}
+	}
+];
